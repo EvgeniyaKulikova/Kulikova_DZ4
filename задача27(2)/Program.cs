4ce782433bd726d73111e9c335dbf89ea56c5ff9
Evgeniya <kulikova.ev94@gmail.com>
2023-02-20 19:38:05 +0300

    задача 27 (2)

---
+++ задача27(2)/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Sumofnumbers (int number)
+{
+    int remainder = 0;
+    int result = 0;
+    while ( number != 0)
+    {
+        remainder = number % 10;
+        result = result + remainder;
+        number = number / 10;
+    }
+  
+    Console.WriteLine($"sum of number a {result}");
+}
+
+Console.Write("imput your number ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Sumofnumbers (userNumber);
+
+
+
+
